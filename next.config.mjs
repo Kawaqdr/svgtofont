3d8c376d0ebfcd8daf5f98f73e035f1dc6192457
Kawaqdr <kawaquader@gmail.com>
2025-11-19 23:27:22 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /*experimental: {
-    appDir: true
-  }*/
+  experimental: {
+    // For Next.js 14 – tell it not to bundle these into server components/route handlers
+    serverComponentsExternalPackages: ["fantasticon", "ttf2woff2"]
+  }
 };
 
 export default nextConfig;
